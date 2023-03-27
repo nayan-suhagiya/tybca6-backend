b7cedcfa3c087ead25ddd6fbfa8d7e5fd17de5cf
nayan-suhagiya <suhagiya.nayan01@gmail.com>
2023-03-27 18:54:37 +0530

    ..

--- router/tblmasterdept.js
+++ router/tblmasterdept.js
@@ -7,5 +7,5 @@ router.post("/admin/add-dept", authAdmin, dept.AddDept);
 router.get("/admin/get-depts", authAdmin, dept.GetDept);
 router.patch("/admin/update-dept", authAdmin, dept.UpdateDept);
 router.delete("/admin/delete-dept/:id", authAdmin, dept.DeleteDept);
-//..
+
 module.exports = router;
