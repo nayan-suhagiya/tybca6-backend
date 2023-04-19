0e7935b6ab56c2139ea92b28951e2bac44b74e6e
nayan-suhagiya <suhagiya.nayan01@gmail.com>
2023-04-19 18:54:11 +0530

    salary pdf generate and send mail using sendgrid

--- router/adminstaff.js
+++ router/adminstaff.js
@@ -33,5 +33,6 @@ router.patch("/admin/reject-staffleave", auth, adminStaff.rejectStaffLeave);
 router.post("/admin/add-salary", auth, adminStaff.addSalary);
 router.get("/admin/get-salary", auth, adminStaff.getSalary);
 router.get("/staff/get-salary/:empid", auth, adminStaff.getSalaryForStaff);
+router.post("/admin/send-mail", auth, adminStaff.sendMail);
 
 module.exports = router;
